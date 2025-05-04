8235ddf5f797dee4c474420761f0d6f4bacc1b77
NodyHody06 <zachshalini44@gmail.com>
2025-05-04 11:01:57 +0530

    Update

--- app.py
+++ app.py
@@ -17,6 +17,6 @@ app.register_blueprint(pages_bp)
    #return app
 
 
-'''if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)'''
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
